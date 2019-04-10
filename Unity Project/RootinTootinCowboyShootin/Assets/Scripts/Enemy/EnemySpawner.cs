5cde4d4b7cc1f7f4425fb253846ff0eaf83d599e
Felix <felixaberg1999@gmail.com>
2019-04-10 16:29:24 +0200

    Slight edit on enemy script

--- Unity Project/RootinTootinCowboyShootin/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Unity Project/RootinTootinCowboyShootin/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public GameObject Enemy;
+    public Sprite[] EnemySprites;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void SpawnEnemy()
     {
-        
+
     }
+
+
 }
