b412f28a128e139e25c1b2b813c53e4e8e464ec3
Felix <felixaberg1999@gmail.com>
2019-04-10 16:26:40 +0200

    Merging stuff
    uwu

--- Unity Project/RootinTootinCowboyShootin/Assets/Scripts/Enemy/EnemyBase.cs
+++ Unity Project/RootinTootinCowboyShootin/Assets/Scripts/Enemy/EnemyBase.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyBase : MonoBehaviour
 {
+    public GameObject reticle;
+
+    //public Vector2 sprite_offset; //Reticle center position offset
+    //public Vector2 hide_position; //Where the enemy hides
+    //public Vector2 exposed_position; //Where the enemy jumps out
+
+
     // Start is called before the first frame update
     void Start()
     {
