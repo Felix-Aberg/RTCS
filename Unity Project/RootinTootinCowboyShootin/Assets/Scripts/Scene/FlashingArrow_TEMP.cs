6d2b806d79d4a033a878b87f6f4d9798d73c8082
Felix <felixaberg1999@gmail.com>
2019-05-24 12:47:19 +0200

    last minute improvements lmao

---
+++ Unity Project/RootinTootinCowboyShootin/Assets/Scripts/Scene/FlashingArrow_TEMP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingArrow_TEMP : MonoBehaviour
+{
+    public Sprite arrow_on;
+    public Sprite arrow_off;
+    bool flashing = false;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        ArrowOn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ArrowOn()
+    {
+        GetComponent<SpriteRenderer>().sprite = arrow_on;
+        Invoke("ArrowOff", 0.3f);
+    }
+
+    void ArrowOff()
+    {
+        GetComponent<SpriteRenderer>().sprite = arrow_off;
+        Invoke("ArrowOn", 0.3f);
+    }
+}
+
